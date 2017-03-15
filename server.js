714b99554b75100245c7814efd2f772d6ee8bd96
kirill <koval4ik2009@gmail.com>
2017-03-15 05:22:49 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const config = require('./config');
+const {userObj} = config;
+
+app.use(express.static(path.join(__dirname, "views")));
+app.use(bodyParser.json());
+app.use(cookieParser);
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+app.post('/login', (req, res) => {
+    console.log(req.body);
+
+    let {username, password} = req.body;
+
+    if (username === userObj.username && password == userObj.password) {
+        res.status(200).json({message: "Authorithed"});
+    } else {
+        res.status(404).json({message: "Wrong login or password"})
+    }
+});
+
+app.listen(config.serverPort, (req, res) => {
+    console.log(`Server running on port ${config.serverPort}`);
+});
