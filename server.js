815d7cd55e217c62bcb76fa742d72cd71ce6a973
kirill <koval4ik2009@gmail.com>
2017-03-19 22:45:09 +0300

    add img view

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 //app.use(express.static(path.join(__dirname, './views/assets')));
 app.use('/static', express.static(__dirname + '/views/assets'));
+app.use('/data', express.static(__dirname + '/store'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -35,26 +36,27 @@ app.use(session({
     saveUninitialized: true
 }));
 app.use(cookieParser(userObj.secret));
-app.use('/ftp', serveIndex(
+app.use('/data', serveIndex(
     './store',
     {
         'icons': true,
         'template': './views/template.html'
     }));
 
-/*app.get('/ftp', (req, res)=> {
- if (req.signedCookies && req.signedCookies.userName) {
- console.log('ok ftp');
- res.send(200);
- } else {
- console.log('no auth');
- res.redirect('/login');
- }
- });*/
+app.get('/data', (req, res) => {
+    if (req.signedCookies && req.signedCookies.userName) {
+        console.log('ok ftp');
+        res.send(200);
+    } else {
+        console.log('no auth');
+        res.redirect('/login');
+    }
+});
+
 
 app.get('/', (req, res) => {
     if (req.signedCookies && req.signedCookies.userName) {
-        res.redirect('/ftp');
+        res.redirect('/data');
     } else {
         res.redirect('/login');
     }
@@ -62,17 +64,17 @@ app.get('/', (req, res) => {
 
 app.get('/login', (req, res) => {
     if (req.signedCookies && req.signedCookies.userName) {
-        res.redirect('/ftp');
+        res.redirect('/data');
     } else {
         res.render('login.html');
     }
 });
 
-app.post('/check_password', (req,res)=> {
+app.post('/check_password', (req, res) => {
     let {password} = JSON.parse(fs.readFileSync('user.json', {encoding: 'utf-8'})),
         checkPassword = req.body.password;
 
-    if(checkPassword && password === checkPassword) {
+    if (checkPassword && password === checkPassword) {
         res.status(200).json({isValid: true});
     } else {
         res.status(400).json({isValid: false});
@@ -120,10 +122,14 @@ app.post('/login', (req, res) => {
             res.cookie('userName', username, options);
         }
 
-        res.redirect('/ftp');
+        res.redirect('/data');
     } else {
-        res.status(400).json({message: "Wrong login or password"})
+        res.redirect('/error-login');
     }
+});
+
+app.get('/error-login', (req, res) => {
+    res.render('error-login.html');
 });
 
 app.post('/upload', (req, res) => {
